cd56dbc60ad6bce56746d21faa98870e1024aa83
ss <>
2025-04-26 19:47:36 +0900

    colelct

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
-
+# settings.py の場所を明示的に設定
+export DJANGO_SETTINGS_MODULE=rest_api.settings # 必要に応じて変更
 pip install -r requirements.txt
-
 python manage.py collectstatic --noinput
 python manage.py migrate
 python manage.py createsuperuser --username admin --email admin@test.com --noinput
