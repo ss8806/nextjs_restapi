bc49840b2515411766ceb4bd113c8b3c58214a95
ss <>
2025-04-26 20:56:16 +0900

    元に戻した

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
+# exit on error
 set -o errexit
-export DJANGO_SETTINGS_MODULE=rest_api.settings
+
 pip install -r requirements.txt
+
 python manage.py collectstatic --noinput
 python manage.py migrate
 python manage.py createsuperuser --username admin --email admin@test.com --noinput
