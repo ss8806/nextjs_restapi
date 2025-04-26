a5b30e696407f0e540654d8e033526e4bf2862d6
ss <>
2025-04-26 20:02:54 +0900

    修正した

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
-# settings.py の場所を明示的に設定
-export DJANGO_SETTINGS_MODULE=rest_api.settings # 修正箇所
+export DJANGO_SETTINGS_MODULE=rest_api.settings
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
